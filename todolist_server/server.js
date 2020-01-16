d78e3429658ec11d149d9b641bc70ce3aad2e5dc
Jonathan Mlynarczyk <jon.mly@gmail.com>
2020-01-16 12:01:20 +0100

    initial commit with server

---
+++ todolist_server/server.js
@@ -0,0 +1,63 @@
+const http = require('http');
+const app = require('./app');
+
+//
+// Actions
+//
+
+const port = normalizePort(process.env.PORT || '8080');
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.on('error', errorHandler);
+server.on('listening', listeningHandler);
+
+server.listen(port);
+
+//
+// Events handlers
+//
+
+const errorHandler = error => {
+    if (error.syscall != 'listen') {
+        throw error;
+    }
+    const bind = getBind();
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' required elevated privileges');
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use');
+            break;
+        default:
+            throw error;
+    }
+}
+
+const listeningHandler = () => {
+    const bind = getBind();
+    console.log('Listening on ' + bind);
+};
+
+//
+// Helpers
+//
+
+const normalizePort = value => {
+    const port = parseInt(value, 10);
+    if (isNaN(port)) {
+        return value;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+}
+
+const getBind = () => {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + address;
+    return bind;
+}
