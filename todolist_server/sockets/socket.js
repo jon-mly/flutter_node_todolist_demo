2c219b2fd749197d2c97d0ac0ed900b0afc5c3d9
Jonathan Mlynarczyk <jon.mly@gmail.com>
2020-01-16 12:58:38 +0100

    added socket handling

---
+++ todolist_server/sockets/socket.js
@@ -0,0 +1,39 @@
+//
+// Configuration
+//
+
+const configureSocketConnection = connection => {
+  console.log("Configuring Socket connection");
+
+  connection.on("connection", onConnection);
+  connection.on("disconnect", onDisconnect);
+  connection.on("error", console.log);
+};
+
+//
+// Connection listeners
+//
+
+const onConnection = socket => {
+  console.log("New client connected : " + socket);
+  addListenersToSocket(socket);
+};
+
+const onDisconnect = socket => {
+  console.log("Client disconnected : " + socket);
+  // TODO: perform actions needed
+};
+
+//
+// Socket configuration
+//
+
+const addListenersToSocket = (socket) => {
+    // TODO: add listeners
+};
+
+//
+// Exportation
+//
+
+exports.configureSocketConnection = configureSocketConnection;
