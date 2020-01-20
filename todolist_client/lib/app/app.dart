1611eb27abdf9b5f2b98e3fea17be0046a76db37
Jonathan Mlynarczyk <jon.mly@gmail.com>
2020-01-20 19:52:09 +0100

    implemented new navigation and LoginPage UI

--- todolist_client/lib/app/app.dart
+++ todolist_client/lib/app/app.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:todolist_client/pages/tasks_page.dart';
-import 'package:todolist_client/providers/app_provider.dart';
+import 'package:todolist_client/pages/login_page.dart';
 import 'package:todolist_client/providers/auth_provider.dart';
-import 'package:todolist_client/providers/tasks_provider.dart';
 
 class App extends StatefulWidget {
   @override
@@ -13,21 +11,11 @@ class App extends StatefulWidget {
 class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        Provider<AppProvider>(
-          create: (_) => AppProvider(),
-        ),
-        Provider<AuthProvider>(
-          create: (_) => AuthProvider(),
-        ),
-      ],
-      child: MaterialApp(
-        theme: ThemeData.dark(),
-        home: ChangeNotifierProvider<TasksProvider>(
-          create: (_) => TasksProvider(),
-          child: TasksPage(),
-        ),
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: ChangeNotifierProvider<AuthProvider>(
+        create: (_) => AuthProvider(),
+        child: LoginPage(),
       ),
     );
   }
--- todolist_client/lib/pages/login_page.dart
+++ todolist_client/lib/pages/login_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todolist_client/pages/tasks_page.dart';
+import 'package:todolist_client/providers/auth_provider.dart';
+import 'package:todolist_client/providers/tasks_provider.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -6,8 +10,98 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  //
+  // ########## ACTIONS
+  //
+
+  Future _login() async {
+    Provider.of<AuthProvider>(context, listen: false)
+        .login(_usernameController.text, _passwordController.text)
+        .then((bool connected) {
+      if (connected) _navigateToTasks();
+    });
+  }
+
+  Future _signup() async {
+    Provider.of<AuthProvider>(context, listen: false)
+        .signup(_usernameController.text, _passwordController.text)
+        .then((bool connected) {
+      if (connected) _navigateToTasks();
+    });
+  }
+
+  void _navigateToTasks() {
+    Navigator.of(context).push(MaterialPageRoute(
+        builder: (_) => ChangeNotifierProvider<TasksProvider>(
+              create: (_) => TasksProvider(),
+              child: TasksPage(),
+            )));
+  }
+
+  //
+  // ########## UI
+  //
+
+  Widget _buildButtonsRow() {
+    return Flex(
+      direction: Axis.horizontal,
+      children: <Widget>[
+        Flexible(
+          flex: 1,
+          child: RaisedButton(
+            child: Text("Login"),
+            onPressed: _login,
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.only(left: 20.0),
+        ),
+        Flexible(
+          flex: 1,
+          child: RaisedButton(
+            child: Text("Signup"),
+            onPressed: _signup,
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildPage() {
+    return Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        children: <Widget>[
+          TextField(
+            controller: _usernameController,
+            decoration: InputDecoration(hintText: "Username"),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 20.0),
+          ),
+          TextField(
+            controller: _passwordController,
+            decoration: InputDecoration(hintText: "Password"),
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 30.0),
+          ),
+          _buildButtonsRow()
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sign In Page"),
+      ),
+      body: _buildPage(),
+    );
   }
 }
